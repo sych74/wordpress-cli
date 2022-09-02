a9cdbbe9be9a04c4b5f88df27bbe0786c51a5e1b
sych74 <sych74@gmail.com>
2022-09-02 14:51:17 +0200

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -11,6 +11,8 @@ wpCommandExec(){
   $WP --path=$WP_PATH $command
 }
 
+echo "" > ${RUN_LOG}
+
 log(){
   local message="$1"
   local timestamp
@@ -34,14 +36,21 @@ execResponse(){
 execAction(){
   local action="$1"
   local message="$2"
-  local isJSON="$3"
 
-  stderr=$( { ${action}; } 2>&1 ) && { log "${message}...done"; execResponse "${SUCCESS_CODE}" "$( ${action} )" "${isJSON}" ; } || {
+  stdout=$( { ${action}; } 2>&1 ) && { log "${message}...done";  } || {
     error="${message} failed, please check ${RUN_LOG} for details"
     execResponse "${FAIL_CODE}" "${error}" "false"
-    log "${message}...failed\n==============ERROR==================\n${stderr}\n============END ERROR================";
+    log "${message}...failed\n==============ERROR==================\n${stdout}\n============END ERROR================";
     exit 0
   }
+}
+
+installWP_CLI(){
+  [ ! -d $HOME/bin ] && mkdir $HOME/bin;
+  curl -s -o $HOME/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar  && chmod +x $HOME/bin/wp;
+  echo "apache_modules:" > ~/bin/wp-cli.yml;
+  echo "  - mod_rewrite" >> ~/bin/wp-cli.yml;
+  ~/bin/wp --info  2>&1;
 }
 
 getCoreVersion(){
@@ -52,13 +61,17 @@ getPluginsList(){
   wpCommandExec 'plugin list --format=json'
 }
 
+execAction "installWP_CLI" 'Install WP-CLI'
+
 case ${1} in
     getVersion)
-        execAction "getCoreVersion" 'Get core version' 'false'
+        execAction "getCoreVersion" 'Get core version'
+        execResponse "${SUCCESS_CODE}" "${stdout}" "false"
         ;;
 
     getPlugins)
         execAction "getPluginsList" 'Get plugins list' 'true'
+        execResponse "${SUCCESS_CODE}" "${stdout}" "true"
         ;;
 
 esac
