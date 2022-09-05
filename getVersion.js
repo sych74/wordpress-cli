edc58916ef728454a9f26dc32fd3863f526e84bb
sych74 <sych74@gmail.com>
2022-09-05 12:37:16 +0200

    Update getVersion.js

--- getVersion.js
+++ getVersion.js
@@ -13,16 +13,16 @@ for (var i = 0, k = envInfo.nodes; i < k.length; i++) {
         nodeId = k[i].id;
 }
 
-var resp = api.env.control.ExecCmdById(envName, session, nodeId, toJSON([{ command: "wp core version --path=/var/www/webroot/ROOT/" }]));
+var resp = api.env.control.ExecCmdById(envName, session, nodeId, toJSON([{ command: "curl --silent https://raw.githubusercontent.com/sych74/wordpress-cli/main/japp.sh > ~/bin/japp.sh && bash ~/bin/japp.sh getVersion" }]));
 
 if (resp.result != 0) return resp;
 
 var scriptResp;
 
 try {
-    scriptResp = resp.responses[0].out;
+    scriptResp = JSON.parse(resp.responses[0].out);
 } catch(ex) {
     scriptResp = { result: Response.ERROR_UNKNOWN, error: ex, resp: resp }
 }
 
-return { result: 0, version: scriptResp };
+return { result: 0, out: scriptResp };
