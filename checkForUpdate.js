d1e0603c670a07e5f93336fee6df158dc9e55761
sych74 <sych74@gmail.com>
2022-09-05 13:55:41 +0200

    Create checkForUpdate.js

---
+++ checkForUpdate.js
@@ -0,0 +1,28 @@
+//@auth
+//@url(wordpress.checkforupdate)
+//@req(envName)
+
+import com.hivext.api.Response;
+
+var envInfo = jelastic.env.control.GetEnvInfo(envName, session);
+
+if (envInfo.result != 0) return envInfo;
+    
+for (var i = 0, k = envInfo.nodes; i < k.length; i++) {
+    if (k[i].nodeGroup == 'cp' && k[i].ismaster)
+        nodeId = k[i].id;
+}
+
+var resp = api.env.control.ExecCmdById(envName, session, nodeId, toJSON([{ command: "curl --silent https://raw.githubusercontent.com/sych74/wordpress-cli/main/japp.sh > ~/bin/japp.sh && bash ~/bin/japp.sh checkForUpdate" }]));
+
+if (resp.result != 0) return resp;
+
+var scriptResp;
+
+try {
+    scriptResp = JSON.parse(resp.responses[0].out);
+} catch(ex) {
+    scriptResp = { result: Response.ERROR_UNKNOWN, error: ex, resp: resp }
+}
+
+return { result: 0, out: scriptResp };
